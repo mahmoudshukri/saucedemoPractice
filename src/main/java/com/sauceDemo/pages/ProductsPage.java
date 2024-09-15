70d0c04b1d172fd2e3f5a3e13b2d7768485e56d1
Mahmoud Shukri <mahmoudshukri.ms@gmail.com>
2024-09-16 00:26:21 +0300

    add Login test case

--- src/main/java/com/sauceDemo/pages/ProductsPage.java
+++ src/main/java/com/sauceDemo/pages/ProductsPage.java
@@ -5,7 +5,12 @@ import org.openqa.selenium.By;
 public class ProductsPage extends BasePage{
     private final By productsHeader = By.xpath("//span[text()='Products']");
     public boolean isProductsHeaderDisplayed(){
+
         return find(productsHeader).isDisplayed();
+    }
+    public String getProductsHeaderDisplayed(){
+
+         return find(productsHeader).getText();
     }
 
 
--- src/test/java/Tests/LoginTests.java
+++ src/test/java/Tests/LoginTests.java
@@ -1,10 +1,13 @@
 package Tests;
 
 import com.sauceDemo.pages.ProductsPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTest{
+    private static final Logger log = LoggerFactory.getLogger(LoginTests.class);
     String userName ="standard_user";
     String inValidPassword="xyz3400";
     String validPassword="secret_sauce";
@@ -23,7 +26,7 @@ public class LoginTests extends BaseTest{
         loginPage.typePassword(validPassword);
         loginPage.clickLoginButton();
         productsPage=new ProductsPage();
-        boolean actualMessage = productsPage.isProductsHeaderDisplayed();
-        Assert.assertTrue(actualMessage,"Product Header is displayed Correctly!!!");
+        String actualMessage = productsPage.getProductsHeaderDisplayed();
+        Assert.assertTrue(actualMessage.contains("Products"));
     }
 }
